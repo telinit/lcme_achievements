f19568508184009f40a5f58fc00d585ebcda3c9f
Genos <sysadmin@lnmo.ru>
2022-05-24 15:04:46 +0400

    Modify models

--- achievements/main_app/admin.py
+++ achievements/main_app/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(OlympiadParticipation)
-admin.site.register(CommonParticipation)
-admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(Department)
+admin.site.register(Education)
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(Seminar)
+admin.site.register(Project)
+admin.site.register(Olympiad)
+admin.site.register(Mark)
+admin.site.register(Award)
+admin.site.register(Participation)
+admin.site.register(User)
 
--- achievements/main_app/models.py
+++ achievements/main_app/models.py
@@ -1,54 +1,58 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     middle_name = models.CharField(max_length=255, blank=True)
     phone_number = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return self.user.__str__()
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+        return f"{self.username}: {self.last_name} {self.first_name} {self.middle_name}"
 
 
 class Location(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Department(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Education(models.Model):
-    student = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     admission_year = models.IntegerField()
     admission_grade = models.IntegerField()
     graduation_year = models.IntegerField()
     graduation_class = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.student}, {self.department}: {self.admission_grade} класс, {self.admission_year} год -- {self.graduation_class} класс, {self.graduation_year} год"
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Activity(models.Model):
     name = models.CharField(max_length=255)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.location}"
 
 
 class Course(Activity):
@@ -58,26 +62,40 @@ class Course(Activity):
     default_departments = models.ManyToManyField(Department)
     default_grade = models.IntegerField()
 
+    def __str__(self):
+        return f"К: {Activity.__str__(self)} - {self.subject} ({self.chapter})"
+
 
 class Seminar(Activity):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"С: {Activity.__str__(self)} - {self.subject}"
 
 
 class Project(Activity):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"П: {Activity.__str__(self)} - {self.subject}"
+
 
 class Olympiad(Activity):
     stage = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"О: {Activity.__str__(self)} - {self.stage}"
+
 
 class Result(models.Model):
-    class Meta:
-        abstract = True
+    pass
 
 
 class Mark(Result):
-    mark = models.CharField(max_length=255)
+    mark_value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.mark_value}"
 
 
 class Award(Result):
@@ -85,21 +103,18 @@ class Award(Result):
     prize = models.CharField(max_length=255)
     is_team_member = models.BooleanField()
 
+    def __str__(self):
+        return f"{self.title}, {self.prize}{', в составе команды' if self.is_team_member else ''}"
+
 
 class Participation(models.Model):
-    student = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    started = models.DateTimeField(auto_now=True)
-    finished = models.DateTimeField(auto_now=True)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    started = models.DateTimeField()
+    finished = models.DateTimeField()
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, null=True)
+    teacher = models.ForeignKey(User, related_name="common_participation_teacher", on_delete=models.CASCADE)
+    result = models.ForeignKey(Result, on_delete=models.CASCADE)
 
-    class Meta:
-        abstract = True
-
-
-class OlympiadParticipation(Participation):
-    award = models.ForeignKey(Award, on_delete=models.CASCADE)
-
-
-class CommonParticipation(Participation):
-    teacher = models.ForeignKey(Profile, related_name="common_participation_teacher", on_delete=models.CASCADE)
-    mark = models.ForeignKey(Mark, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.activity} - {self.student}"
 
